70df678b5973df31ae8c03ed2b6d441b659cafbc
Zeyad090 <zeyad.altwalah@gamil.com>
2022-01-04 20:36:46 +0300

    Add Services From Firebase

--- app/src/main/java/zeyad/app/aallyalsohoobelevators/request/RequestFragment.kt
+++ app/src/main/java/zeyad/app/aallyalsohoobelevators/request/RequestFragment.kt
@@ -1,6 +1,7 @@
 package zeyad.app.aallyalsohoobelevators.request
 
 import android.app.Activity
+import android.media.Image
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,10 +10,16 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import com.google.rpc.Help
+import zeyad.app.aallyalsohoobelevators.R
 import zeyad.app.aallyalsohoobelevators.databinding.FragmentRequestBinding
+import zeyad.app.aallyalsohoobelevators.services.ServicesViewModel
 
 
 class RequestFragment : Fragment() {
+    private  val viewModel: RequestViewModel by viewModels()
    lateinit var binding: FragmentRequestBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,8 +32,15 @@ class RequestFragment : Fragment() {
     ): View? {
        binding = FragmentRequestBinding.inflate(inflater,container,false)
         return binding.root
+
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.send.setOnClickListener {
+            Toast.makeText(requireContext(),"your request sent successfully",Toast.LENGTH_SHORT).show()
+        }
+    }
 
 
  }
--- app/src/main/java/zeyad/app/aallyalsohoobelevators/services/ServicesFragment.kt
+++ app/src/main/java/zeyad/app/aallyalsohoobelevators/services/ServicesFragment.kt
@@ -1,29 +1,91 @@
 package zeyad.app.aallyalsohoobelevators.services
 
+
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
+import androidx.fragment.app.viewModels
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import zeyad.app.aallyalsohoobelevators.R
 import zeyad.app.aallyalsohoobelevators.databinding.FragmentServicesBinding
+import zeyad.app.aallyalsohoobelevators.services.data.DataFirebaseServicesContracts
+import kotlin.math.log
+import kotlin.reflect.typeOf
 
 
 class ServicesFragment : Fragment() {
+    private val viewModel: ServicesViewModel by viewModels()
     lateinit var binding: FragmentServicesBinding
+    // lateinit var  spinner :Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.e("TAG", "onCreate: start", )
+
 
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentServicesBinding.inflate(inflater,container,false)
+        binding = FragmentServicesBinding.inflate(inflater, container, false)
         return binding.root
+
+
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+       getContract1()
+
+
+
+      //  binding.fixSpinner.adapter = ArrayAdapter(requireContext(),
+        //    android.R.layout.simple_spinner_item,viewModel.contractslist.value!!)
+
+
+        binding.onTime.setOnClickListener {
+            Toast.makeText(requireContext(), "your request sent successfully", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+
+
+    fun getContract1() {
+        val db = Firebase.firestore
+        val list: MutableCollection<DataFirebaseServicesContracts>
+        db.collection("Contracts").document("Contracts")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result.data!!.values) {
+
+                    Log.e("TAG", "real:${result.data!!.values}")
+                    binding.fixSpinner.adapter = ArrayAdapter(requireContext(),android.R.layout.simple_spinner_item
+                        ,result!!.data?.values!!.toTypedArray() )
+
+                }
+
+            }
+            .addOnFailureListener { exception ->
+                Log.w("TAG", "Error getting documents.", exception)
+            }
+    }
+
+
+
+
 }
+
+
